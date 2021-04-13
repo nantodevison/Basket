65f0a1a4be15891efdc95ba857d4ff500a7e8ce9
martin.schoreisz <martin.schoreisz@.cerema.fr>
2021-04-13 21:41:51 +0200

    test avec thread et move to.
    ok, mais 'NoneType' object has no attribute 'keys'

--- Basket/src/Ihm/MainWindow.py
+++ Basket/src/Ihm/MainWindow.py
@@ -12,6 +12,8 @@ import pandas as pd
 from Ihm.Initialiser_donnees_IHM import lastDates, nbJourneeImportDefaut
 from TeleversementJourneeBdd import JourneeBdd
 from datetime import date
+from PyQt5.QtCore import QObject
+from time import sleep
 
 
 class WindowPrincipale(QtWidgets.QMainWindow):
@@ -49,10 +51,28 @@ class WindowPrincipale(QtWidgets.QMainWindow):
             date : string : date de depart de telechargement (YYYY-MM-DD)
             duree : integer : nb de jours a telecharegre a partir de la date de depart
         """
-        print('avant fonction Tread')
-        self.telecharge=WorkerTelechargement(self.dateEdit_ImportJournee.date().toString('yyyy-MM-dd'),
+        print('avant ouverture Tread')
+        self.thread = QThread()
+        print('avant creation worker')
+        self.workerTelechargement=WorkerTelechargement(self.dateEdit_ImportJournee.date().toString('yyyy-MM-dd'),
                                              self.spinBox_NbjourImport.value())
-        self.telecharge.start()
+        #self.workerTelechargement=WorkerTelechargement()
+        print('avant move to')
+        self.workerTelechargement.moveToThread(self.thread)
+        print('avant signaux/slots')
+        self.thread.started.connect(self.workerTelechargement.run)
+        self.workerTelechargement.finished.connect(self.thread.quit)
+        self.workerTelechargement.finished.connect(self.workerTelechargement.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        #self.workerTelechargement.progress.connect(self.reportProgress)
+        # Step 6: Start the thread
+        self.thread.start()
+    """ 
+    @pyqtSlot(int)   
+    def reportProgress(self, cnt):
+        print(cnt)
+    """
+
 
 class BarreProgression(QtWidgets.QDialog):
     """
@@ -63,36 +83,51 @@ class BarreProgression(QtWidgets.QDialog):
         super(BarreProgression, self).__init__()
         uic.loadUi(r'C:\Users\martin.schoreisz\git\Basket\Basket\src\Ihm\ProgressBar.ui', self)
             
-class WorkerTelechargement(QThread):
+class WorkerTelechargement(QObject):
     """
     class pour executer les actions de telechargement / televersement des donnees
     """
+    finished = pyqtSignal()
     signalNbMatch=pyqtSignal(int)#envoyer le nb total de la journee en cours
     signalNbJournee=pyqtSignal(int)#envoyer le nb total de journee
     signalNouvelleJournee=pyqtSignal(int)#pour envoyer le numero de la journee telechragee
     
     def __init__(self, date_debut, duree):
-        super(WorkerTelechargement, self).__init__()
+        super(WorkerTelechargement,self).__init__()
         self.date_debut=date_debut
         self.duree=duree
         print(self.date_debut,self.duree )
     
     def run(self):
-        """
-        la fonction de telechargement des donnees
-        """
         print('thread run')
-        listeDate=[d.strftime('%Y-%m-%d') for d in pd.date_range(start=self.date_debut,
-                                                                periods=self.uree)]
-        self.signalNbJournee.emit(len(listeDate))
+        try :
+            listeDate=[d.strftime('%Y-%m-%d') for d in pd.date_range(start=self.date_debut,
+                                                                    periods=self.duree)]
+            print(listeDate)
+            self.signalNbJournee.emit(len(listeDate))
+            print('avant boucle')
         
-        for e,j in  enumerate(listeDate): 
-            self.signalNouvelleJournee.emit(e+1)
-            print(j)
-            journee=JourneeBdd(j)
-            self.signalNbMatch.emit(journee.nbMatchs)
-            journee.creerAttributsGlobaux()
-            #journee.exporterVersBdd()
+            for e,j in  enumerate(listeDate): 
+                self.signalNouvelleJournee.emit(e+1)
+                print(j)
+                journee=JourneeBdd(j)
+                self.signalNbMatch.emit(journee.nbMatchs)
+                journee.creerAttributsGlobaux()
+                #journee.exporterVersBdd()
+        except Exception as e : 
+            print(e)
+        self.finished.emit()
+    """
+    finished = pyqtSignal()
+    progress = pyqtSignal(int)
+
+    def run(self):
+        for i in range(5):
+            print(i)
+            sleep(1)
+            self.progress.emit(i + 1)
+        self.finished.emit()
+    """
         
 if __name__=="__main__" : 
     import sys 
